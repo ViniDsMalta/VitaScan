f02c583bb6693708aedf5babd324c4ae5da7828f
Vinícius Malta <viniciusmalta2503@gmail.com>
2025-11-03 22:18:58 -0300

    1 linha

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,7 @@ const config = {
 
   apiUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
-    : 'https://seu-backend.up.railway.app'  
+    : 'https://vitascan-backend-axs2.onrender.com'  
 };
 
 
