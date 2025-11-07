88325f113db874c1d1dd86e908c5a415d18a0466
Vinícius Malta <viniciusmalta2503@gmail.com>
2025-11-07 16:30:27 -0300

    acho que resolveu 4

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const User = require('./models/User');
 const Triagem = require('./models/Triagens');
 
 const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
 app.use(cors({
   origin: [
     'http://localhost:8080',
